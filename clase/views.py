d5458716286597cd7f591fc2999dcc962a2d228f
Cristian D Biancotti <cdbiancotti@gmail.com>
2022-03-07 19:50:18 -0300

    cambios para poder ver conflictos

--- clase/views.py
+++ clase/views.py
@@ -9,4 +9,5 @@ def nuevo_curso(request):
     camada = random.randrange(1500, 3000)
     nuevo_curso = Curso(nombre='Curso Pyhon', camada=camada)
     nuevo_curso.save()
+    5-5
     return HttpResponse(f"COSA RARA AGREGADA - Se creo el curso {nuevo_curso.nombre} camada {nuevo_curso.camada}")
