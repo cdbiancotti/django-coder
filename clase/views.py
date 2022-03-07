48aaa7c5144c4a7a429ed4bcdaa9091246a2e277
Cristian D Biancotti <cdbiancotti@gmail.com>
2022-03-07 19:43:46 -0300

    Cambio en la otra pc

--- clase/views.py
+++ clase/views.py
@@ -9,4 +9,4 @@ def nuevo_curso(request):
     camada = random.randrange(1500, 3000)
     nuevo_curso = Curso(nombre='Curso Pyhon', camada=camada)
     nuevo_curso.save()
-    return HttpResponse(f"Se creo el curso {nuevo_curso.nombre} camada {nuevo_curso.camada}")
+    return HttpResponse(f"COSA RARA AGREGADA - Se creo el curso {nuevo_curso.nombre} camada {nuevo_curso.camada}")
